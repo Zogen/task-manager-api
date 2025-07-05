463ca7e4d3813a24be999bcefece82c48c31d945
Georgios Ziogas <ziogasgk@csd.auth.gr>
2025-07-05 14:26:02 +0300

    added initial dependencies

---
+++ src/test/java/com/zogen/taskmgr/TaskMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zogen.taskmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
